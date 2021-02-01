98575e339aac3b1920050ceee2d198aaf380d77d
ecastan960 <ecastan1@eafit.edu.co>
2021-02-01 02:58:09 -0500

    04|fix effectivness

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,29 +7,29 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i = 0, c = 0, flag = 0;
+	size_t i = 0, last = size;
 	int temp = 0;
 
 	if (array == NULL)
 		return;
 
-	while (c == 0)
+	while (i < last)
 	{
-		flag = 0;
-		for (i = 0; i < size; i++)
+		if (array[i + 1] < array[i] && i < size - 1)
 		{
-			if (array[i + 1] < array[i] && i < size - 1)
+			temp = array[i + 1];
+			array[i + 1] = array[i];
+			array[i] = temp;
+			if (i + 1 == last - 1)
 			{
-				temp = array[i + 1];
-				array[i + 1] = array[i];
-				array[i] = temp;
-				print_array(array, size);
-				flag = 1;
+				last--;
+				i = 0;
 			}
-			if (i == size - 1 && flag == 0)
-			{
-				c = 1;
-			}
+			else
+				i++;
+			print_array(array, size);
 		}
+		else
+			i++;
 	}
 }
