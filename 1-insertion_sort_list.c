f6c8356a0796c1605773ae4bc1d8551900310792
ecastan960 <ecastan1@eafit.edu.co>
2021-02-01 09:25:20 -0500

    18|fix edge cases

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,7 +9,7 @@ void insertion_sort_list(listint_t **list)
 	listint_t *tmp = *list;
 	listint_t *head = *list;
 
-	if (*list == NULL || list == NULL)
+	if (*list == NULL || list == NULL || (*list)->next == NULL)
 		return;
 
 	while ((*list)->next != NULL)
